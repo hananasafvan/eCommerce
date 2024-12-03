0e453189c42780dc31a3026563713c4988d148ab
Nidha Hanan BCR63 <hananasafvan@gmail.com>
2024-12-03 13:38:14 +0530

    befor restart pm2 and ngnx

--- controllers/user/paymentController.js
+++ controllers/user/paymentController.js
@@ -18,8 +18,8 @@ const payproduct = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "nidha.live/success",
-        cancel_url: "nidha.live/cancel",
+        return_url: "http://nidha.live/success",
+        cancel_url: "http://nidha.live/cancel",
       },
       transactions: [
         {
